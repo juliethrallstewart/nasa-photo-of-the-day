74d23c2d1fb868d16adcf418cc62ce351b98cd0b
Julie Thrall <juliethrall10@gmail.com>
2019-07-17 19:12:24 -0700

    fixed prop passing between Data.js/Media.js, added Post.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import Data from '../src/components/Data/Data.js';
-import Media from '../src/components/Media/Media.js';
 
 function App () {
 	return (
 		<div className="App">
-			{/* <p>Confusion is nothing new</p> */}
 			<Data />
 		</div>
 	);
--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Media from '../Media/Media.js';
+import Post from '../Post/Post.js';
+
+const nasaAPI = 'https://api.nasa.gov/planetary/apod?api_key=nfIXoKXfXb5pGxmbanV70k4dOopcTy9kW8U05nH8';
 
 export default function Data ({ limit }) {
+	const [
+		API,
+		setAPI
+	] = useState(nasaAPI);
+
+	const [
+		content,
+		setContent
+	] = useState('');
+
 	const [
 		date,
 		setDate
@@ -24,29 +37,37 @@ export default function Data ({ limit }) {
 		setUrl
 	] = useState();
 
-	useEffect(() => {
-		axios
-			.get('https://api.nasa.gov/planetary/apod?api_key=nfIXoKXfXb5pGxmbanV70k4dOopcTy9kW8U05nH8')
-			.then((res) => {
-				const result = res.data;
-				setDate(result.date);
-				setExplanation(result.explanation);
-				setMediaType(result.media_type);
-				setTitle(result.title);
-				setUrl(result.url);
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	}, []);
+	useEffect(
+		() => {
+			axios
+				.get(API)
+				.then((res) => {
+					const result = res.data;
+					setContent(result);
+					setDate(result.date);
+					setExplanation(result.explanation);
+					setMediaType(result.media_type);
+					setTitle(result.title);
+					setUrl(result.url);
+				})
+				.catch((e) => {
+					console.log(e);
+				});
+		},
+		[
+			API
+		]
+	);
 
 	console.log(url);
 
 	return (
 		<div className="container">
 			<h1>Nasa Photo of the Day</h1>
-			{/* <Media url={url} /> */}
-			<Media />
+			{
+				content !== '' ? <Post promise={content} /> :
+				<div>Loading...</div>}
+			<Media imgUrl={url} />
 			<h2>{title}</h2>
 			<p>{date}</p>
 			<p>{explanation}</p>
